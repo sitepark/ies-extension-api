12ba08949b427937294e9ffb4cb07d02abe0dea6
Holger Veltrup <92872893+sitepark-veltrup@users.noreply.github.com>
2023-11-02 16:21:26 +0100

    refactor: add Exception-suffix for exception-classes (#45)
    * refactor: add Exception-suffix for exception-classes
    
    * chore: update pom.xml
    
    * fix: javadoc errors

--- src/main/java/com/sitepark/ies/extension/api/exeptions/InvalidExtensionClassException.java
+++ src/main/java/com/sitepark/ies/extension/api/exeptions/InvalidExtensionClassException.java
@@ -2,18 +2,18 @@ package com.sitepark.ies.extension.api.exeptions;
 
 import com.sitepark.ies.extension.api.Extension;
 
-public class InvalidExtensionClass extends ExtensionException {
+public class InvalidExtensionClassException extends ExtensionException {
 
 	private final Class<? extends Extension> extensionClass;
 
 	private static final long serialVersionUID = 1L;
 
-	public InvalidExtensionClass(Class<? extends Extension> extensionClass, String message) {
+	public InvalidExtensionClassException(Class<? extends Extension> extensionClass, String message) {
 		super(message);
 		this.extensionClass = extensionClass;
 	}
 
-	public InvalidExtensionClass(Class<? extends Extension> extensionClass, String message, Throwable t) {
+	public InvalidExtensionClassException(Class<? extends Extension> extensionClass, String message, Throwable t) {
 		super(message, t);
 		this.extensionClass = extensionClass;
 	}
