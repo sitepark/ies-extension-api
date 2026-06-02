6dd6135e063144236ee3a0e60af1df272376b5c7
Holger Veltrup <veltrup@sitepark.com>
2026-06-02 17:53:55 +0200

    feat: add ClassFinder.findByAnnotation() and ClassFinder.loadClass()

--- src/main/java/com/sitepark/ies/extension/api/ClassFinder.java
+++ src/main/java/com/sitepark/ies/extension/api/ClassFinder.java
@@ -1,10 +1,18 @@
 package com.sitepark.ies.extension.api;
 
+import java.lang.annotation.Annotation;
 import java.util.List;
 
 public interface ClassFinder {
 
   <T> List<Class<? extends T>> findBySubType(Class<T> type);
+
+  <A extends Annotation> List<Class<?>> findByAnnotation(Class<A> annotation);
+
+  <T, A extends Annotation> List<Class<? extends T>> findByAnnotation(
+      Class<T> type, Class<A> annotation);
+
+  Class<?> loadClass(String className) throws ClassNotFoundException;
 
   void initialize();
 
